f1a68b8302a87c0c4305e98578da91d602539887
Alex <webmaster@papagai.de>
2022-01-31 19:30:56 +0100

    fix

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ switch (true) {
     console.log(dayArr[0].toString());
     break
 
-  case lang == "en" :
+  case lang == "en":
     console.log(dayArr[1].join());
     break
 
@@ -43,13 +43,23 @@ switch (true) {
 
 let langArr = lang == "ru" ? console.log(dayArr[0].join()) : console.log(dayArr[1].join());
 
+//  Или другой вариант:
+
+let dayArr1 = {
+  "ru": 'Понедельник, Вторник, Среда, Четвер, Пятница, Суббота, Воскресенье',
+
+  "en": 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday'
+};
+console.log(dayArr1[lang]);
+
 
 //2). У нас есть переменная namePerson. Если значение этой переменной “Артем” то вывести в консоль “директор”, если значение “Александр” то вывести в консоль “преподаватель”, с любым другим значением вывести в консоль “студент”
 
- //Решить задачу с помощью нескольких тернарных операторов, без использования if или switch
+//Решить задачу с помощью нескольких тернарных операторов, без использования if или switch
 
-let namePerson ="Александр"
+let namePerson = "Александр"
 
 let inputName1 = namePerson == "Артем" ? console.log("Директор.") : namePerson == "Александр" ? console.log("Преподаватель.") : console.log("Студент.");
+
 
 
