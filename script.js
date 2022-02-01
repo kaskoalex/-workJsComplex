c43a2430d015bfb0f0a4c02343922ee74c066eaf
Alex <webmaster@papagai.de>
2022-02-01 21:04:18 +0100

    create: lesson04

--- script.js
+++ script.js
@@ -1,62 +1,21 @@
 "use strict";
 
-//1). Переменная lang может принимать 2 значения: 'ru' 'en'.
 
 
+const stringValidation = function (str) {
+  if (typeof str !== typeof str.toString()) {
+    alert("Заданный тип данных не является строкой");
+  } else {
+    str = str.trim();
+    if (str.length <31){
+      return str
+    } else {
+      return str=str.slice(0,30)+"..."
+    }
+  }
 
-
-let dayArr = [
-  ['ru','en'],
-
-  ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-
-  ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-];
-
-let lang = "en";
-// a) через if  -----------------------------
-
-if (lang == "ru") {
-  console.log(dayArr[1].toString());
-
-} else if (lang == "en") {
-  console.log(dayArr[2].join());
-
-} else {
-  console.log("Something went wrong");
 }
 
-
-
-
-//b) через switch-case -----------------------
-
-switch (true) {
-  case lang == "ru":
-    console.log(dayArr[1].toString());
-    break
-
-  case lang == "en":
-    console.log(dayArr[2].join());
-    break
-
-  default:
-    console.log("Something went wrong");
-}
-
-//c) через многомерный массив без ифов и switch.
-
-let language = dayArr[0].indexOf(lang);
-console.log(dayArr[language + 1].join())
-
-
-//2). У нас есть переменная namePerson. Если значение этой переменной “Артем” то вывести в консоль “директор”, если значение “Александр” то вывести в консоль “преподаватель”, с любым другим значением вывести в консоль “студент”
-
-//Решить задачу с помощью нескольких тернарных операторов, без использования if или switch
-
-let namePerson = "Александр"
-
-let inputName1 = namePerson == "Артем" ? console.log("Директор.") : namePerson == "Александр" ? console.log("Преподаватель.") : console.log("Студент.");
-
-
-
+console.log(stringValidation("0123456789012345678901234567890123456789"))
+console.log(stringValidation("     1234567"))
+console.log(stringValidation(23456789012))
