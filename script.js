78c083a98ea4a77eb9f5e347f971fa81dd37d5cd
Alex <webmaster@papagai.de>
2022-02-03 02:17:48 +0100

    create lesson05

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 "use strict";
 
 
+let flag = 0
+let arr = ['41222', '12255556', '26587425', '7852136', '42222222', '2011444', '36587458'];
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i][0] === "2" || arr[i][0] === "4") {
+    console.log(arr[i]);
+    flag++
 
-const stringValidation = function (str) {
-  if (typeof str !== 'string') {
-    alert("Заданный тип данных не является строкой");
-  } else {
-    strNew = str.trim();
-    if (strNew.length <31){
-      return strNew
-    } else {
-      return strNew.slice(0,30)+"..."
-    }
   }
-
+}
+if (flag === 0) {
+  console.log("Таких чисел нет")
 }
 
-console.log(stringValidation("0123456789012345678901234567890123456789"))
-console.log(stringValidation("     1234567"))
-console.log(stringValidation(23456789012))
+
+
+let n = 100
+nextPrime:
+for (let i = 2; i <= 100; i++) {
+
+  for (let j = 2; j < i; j++) {
+    if (i % j == 0) continue nextPrime;
+  }
+
+  console.log(i + " Делители этого числа: 1 и " + i);
+}
